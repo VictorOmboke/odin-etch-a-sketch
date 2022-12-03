6f3578947318613da49fee80428b01e17493897f
Victor Omboke <victoromboke@gmail.com>
2022-12-02 17:55:17 -0800

    Add event listener to makeGrid function to create sketch effect

--- eas.js
+++ eas.js
@@ -1,15 +1,19 @@
-//name container in DOM
+//name DOM elements
 const container = document.querySelector('#container');
+let gridSize = prompt('Please enter grid size:', 16);
 
-//Function that will create a grid
-function makeGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (let i = 0; i < (rows * cols); i++) {
+//Function that will create a grid (inp = input) and add hover effect for cells
+function makeGrid(inp) {
+    container.style.setProperty('--grid-rows', inp);
+    container.style.setProperty('--grid-cols', inp);
+    for (let i = 0; i < (inp * inp); i++) {
         let cell = document.createElement('div');
         cell.classList.add('grid-item');
         container.appendChild(cell);
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        });
     };
 };
 
-makeGrid(16, 16);
+makeGrid(gridSize);
